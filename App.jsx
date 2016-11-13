f5a90d9911532260c973ede793e547c785f84e65
havietduc91 <havietduc91@gmail.com>
2016-11-13 10:27:58 +0700

    ReactJS - JSX

--- App.jsx
+++ App.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 
 class App extends React.Component {
     render() {
+        var myStyle = {
+            fontSize: 100,
+            color: '#FF0000'
+        };
+
         return (
             <div>
-                Hello World!!!
+                <h1 style = {myStyle}>Header</h1>
+                <h1>{1+1}</h1>
+                <h1>Header</h1>
+                <h2>Content</h2>
+                <p data-myattribute = "somevalue">This is the content!!!</p>
+                {//End of the line Comment...}
+                {/*Multi line comment...*/}
             </div>
         );
     }

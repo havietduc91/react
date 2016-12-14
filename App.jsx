445787a9c1b61461ea19c2247c1e3646104c700a
havietduc91 <havietduc91@gmail.com>
2016-12-14 11:16:55 +0700

    Add react table

--- App.jsx
+++ App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Reactable from 'reactable';
 var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 class App extends React.Component {
@@ -8,24 +9,24 @@ class App extends React.Component {
 
         this.state = {
             items: ['Item 1...', 'Item 2...', 'Item 3...', 'Item 4...']
-        }
+        };
 
         this.handleAdd = this.handleAdd.bind(this);
     };
 
     handleAdd() {
-        var newItems = this.state.items.concat([prompt('Create New Item')]);
+        let newItems = this.state.items.concat([prompt('Create New Item')]);
         this.setState({items: newItems});
     }
 
     handleRemove(i) {
-        var newItems = this.state.items.slice();
+        let newItems = this.state.items.slice();
         newItems.splice(i, 1);
         this.setState({items: newItems});
     }
 
     render() {
-        var items = this.state.items.map(function(item, i) {
+        let items = this.state.items.map(function(item, i) {
             return (
                 <div key = {item} onClick = {this.handleRemove.bind(this, i)}>
                     {item}
@@ -33,6 +34,8 @@ class App extends React.Component {
             );
 
         }.bind(this));
+
+        let Table = Reactable.Table;
 
         return (
             <div>
@@ -42,6 +45,27 @@ class App extends React.Component {
                                          transitionEnterTimeout = {500} transitionLeaveTimeout = {500}>
                     {items}
                 </ReactCSSTransitionGroup>
+
+                <Table className="table" id="table" data={[
+                    { Name: 'Lee Salminen', Age: '23', Position: 'Programmer'},
+                    { Name: 'Griffin Smith', Age: '18', Position: 'Engineer'},
+                    { Name: 'Ian Zhang', Age: '28', Position: 'Developer'}
+                ]}
+                   sortable={[
+                       {
+                           column: 'Name',
+                           sortFunction: function(a, b){
+                               // Sort by last name
+                               let nameA = a.split(' ');
+                               let nameB = b.split(' ');
+
+                               return nameA[1].localeCompare(nameB[1]);
+                           }
+                       },
+                       'Age',
+                       'Position'
+                   ]}
+                   defaultSort={{column: 'Age', direction: 'desc'}}/>
             </div>
         );
     }

0c7ad715d0ea4aaedc9619ee5d3d01c6cd4c5811
Do Hoang Lam <lamdh@zergitas.com>
2016-12-17 10:43:09 +0700

    Commit the first

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
+
 import App from './App.jsx'
 import todoApp from './reducers/reducers'
 
